394afb04c6acfd58620384e087faa174b3b8134f
kretika arora <kretikaarora@gmail.com>
2020-09-29 13:31:56 +0530

    WELCOME TO EMPLOYEE COMPUTATION PROGRAM

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DailyWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+            
+        }
+    }
+}
